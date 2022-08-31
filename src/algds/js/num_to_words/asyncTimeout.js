9b62482199a5d0569195de2b3a494f11ae565796
Fernando Basso <fernandobasso.br@gmail.com>
2022-08-31 08:23:49 -0300

    aglds(js): Implement num to words
    Implement an algorithm that converts a number to its written form.

---
+++ src/algds/js/num_to_words/asyncTimeout.js
@@ -0,0 +1,19 @@
+const { promisify } = require('util')
+
+const asyncTimeout = promisify(setTimeout)
+
+async function run() {
+  console.log('bar')
+
+  await asyncTimeout(1000)
+
+  console.log('foo')
+}
+
+run()
+
+console.log('baz')
+
+JWT
+
+\p
