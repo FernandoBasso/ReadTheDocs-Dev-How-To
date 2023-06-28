5b017e70aacea4b80f95b96464774f73ed4996d6
Fernando Basso <fernandobasso.br@gmail.com>
2023-06-28 07:24:17 -0300

    dbsql: Dump SimplySQL teamsgames DB

---
+++ src/databases-sql/dumps/db_simplysql_teamsgames_devel_bkp_latest.sql
@@ -0,0 +1,75 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.8 (Debian 14.8-1.pgdg120+1)
+-- Dumped by pg_dump version 15.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: devel
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO devel;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: teams; Type: TABLE; Schema: public; Owner: devel
+--
+
+CREATE TABLE public.teams (
+    id integer NOT NULL,
+    name character varying(37) NOT NULL,
+    conference character varying(2)
+);
+
+
+ALTER TABLE public.teams OWNER TO devel;
+
+--
+-- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: devel
+--
+
+COPY public.teams (id, name, conference) FROM stdin;
+37	Havoc	F
+63	Brewers	C
+9	Riff Raff	E
+\.
+
+
+--
+-- Name: teams teams_pkey; Type: CONSTRAINT; Schema: public; Owner: devel
+--
+
+ALTER TABLE ONLY public.teams
+    ADD CONSTRAINT teams_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: devel
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
