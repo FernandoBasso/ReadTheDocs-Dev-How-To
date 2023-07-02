55a0296cc8a9d4ad5eee92a2d5bc2285fa2eb3d2
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-02 15:27:57 -0300

    dbsql: Add backup DBs

--- src/databases-sql/dbdumps/simplysql_jointypes_devel-bkp.sql
+++ src/databases-sql/dbdumps/simplysql_jointypes_devel-bkp.sql
@@ -17,6 +17,48 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: simplysql_jointypes_devel; Type: DATABASE; Schema: -; Owner: devel
+--
+
+CREATE DATABASE simplysql_jointypes_devel WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.UTF-8';
+
+
+ALTER DATABASE simplysql_jointypes_devel OWNER TO devel;
+
+\connect simplysql_jointypes_devel
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: simplysql_jointypes_devel; Type: DATABASE PROPERTIES; Schema: -; Owner: devel
+--
+
+ALTER DATABASE simplysql_jointypes_devel CONNECTION LIMIT = 3;
+
+
+\connect simplysql_jointypes_devel
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: public; Type: SCHEMA; Schema: -; Owner: devel
 --
 
