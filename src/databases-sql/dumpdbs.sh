e02765d7e80930ed067abfee9e9566db0fd23c47
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-02 15:27:47 -0300

    dbsql: Add simple script to dump existing DBs

---
+++ src/databases-sql/dumpdbs.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#
+# Dumps databases including their DDL code.
+#
+# NOTE: This is a simple script to dump my study databases. All the DBs
+# must belong to the same user and with the the same password.
+#
+# Usage:
+#
+#   PGHOST=<host> \
+#     PGUSER=<user> \
+#     PGPASSWORD='h4ck3r' \
+#     bash dump_dbs.sh
+#
+# BEWARE: The password will be saved on your shell history. You may
+# start the dump command with a space to cause bash to NOT save the
+# command to the history, but keep in mind that it depends on some
+# bash's configs (should be on by default, though, unless you changed
+# it).
+#
+
+##
+# Keep this array updated with whatever databases need to be backed up
+# from time to time.
+#
+# BEWARE: Even though this is for local, study databases, be careful
+# with sensitive information as the dumped DBs are committed to the repo
+# as well.
+#
+dbs=(
+	simplysql_cms_devel
+	simplysql_teamsgames_devel
+	simplysql_jointypes_devel
+)
+
+dumpsdir=./dbdumps
+
+mkdir -pv "$dumpsdir"
+
+for db in "${dbs[@]}"
+do
+	outfile="${dumpsdir}/${db}.sql"
+	outbkpfile=""
+
+	##
+	# Make a backup of the last backup in case the new backup
+	# being made fails for some reason.
+	#
+	if [[ -r $outfile ]] ; then
+		mv -v "$outfile" "${outfile%.sql}-bkp.sql"
+	fi
+
+	##
+	# PGHOST, PGUSER and PGPASSWORD must be provided as env vars.
+	#
+	PGDATABASE="$db" pg_dump \
+		--create --file "$outfile" && \
+		printf "• File created: '%s'\n" "$outfile"
+done
+
+# vim: set tw=72:
