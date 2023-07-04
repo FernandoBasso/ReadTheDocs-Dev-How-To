81c1c595c1ba5576e644fcf3579e3d9e7fab2ff5
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-04 08:50:30 -0300

    dbsql: Add categories to cms DB

--- src/databases-sql/dbdumps/simplysql_cms_devel.sql
+++ src/databases-sql/dbdumps/simplysql_cms_devel.sql
@@ -63,6 +63,18 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: categories; Type: TABLE; Schema: public; Owner: devel
+--
+
+CREATE TABLE public.categories (
+    category character varying(9) NOT NULL,
+    name character varying(37) NOT NULL
+);
+
+
+ALTER TABLE public.categories OWNER TO devel;
+
+--
 -- Name: entries; Type: TABLE; Schema: public; Owner: devel
 --
 
@@ -79,6 +91,19 @@ CREATE TABLE public.entries (
 ALTER TABLE public.entries OWNER TO devel;
 
 --
+-- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: devel
+--
+
+COPY public.categories (category, name) FROM stdin;
+blog	Log on to My Blog
+humor	Humorous Anecdotes
+angst	Stories from the Id
+advice	Gentle Words of Advice
+science	Our Spectacular Universe
+\.
+
+
+--
 -- Data for Name: entries; Type: TABLE DATA; Schema: public; Owner: devel
 --
 
@@ -89,6 +114,14 @@ COPY public.entries (id, title, created, updated, category, content) FROM stdin;
 598	The Size of Our Galaxy	2009-04-03 00:00:00	\N	science	\N
 524	Uncle Karl and the Gasoline	2009-02-28 00:00:00	\N	humor	When I was about nine or ten, my Uncle Karl, who\nwould've been in his late teens or early twenties, once performed\nwhat to me seemed like a magic trick.\n\nUsing a rubber hose, which he snaked down into the gas tank of my \nfather's car, he siphoned some gasoline into his mouth, lit a match, \nheld it up a few inches in front of his face, and then, with explosive \nforce, sprayed the gasoline out towards the lit match.\n\nOf course, a huge fireball erupted, much to the delight of the kids\nwatching. I don't recall if he did it more than once.\n\nThe funny part of this story? We lived to tell it.\n\nKarl was like that.
 \.
+
+
+--
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: devel
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (category);
 
 
 --
