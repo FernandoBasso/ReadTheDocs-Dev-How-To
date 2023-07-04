37bd7141745d321c52db0631297719df9718d32d
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-04 07:11:59 -0300

    dbsql: Dump dbs with proper line endings

--- src/databases-sql/dbdumps/simplysql_cms_devel-bkp.sql
+++ src/databases-sql/dbdumps/simplysql_cms_devel-bkp.sql
@@ -1,1 +1,105 @@
-pg_dump: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "root" does not exist
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.8 (Debian 14.8-1.pgdg120+1)
+-- Dumped by pg_dump version 14.8 (Debian 14.8-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: simplysql_cms_devel; Type: DATABASE; Schema: -; Owner: devel
+--
+
+CREATE DATABASE simplysql_cms_devel WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.UTF-8';
+
+
+ALTER DATABASE simplysql_cms_devel OWNER TO devel;
+
+\connect simplysql_cms_devel
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: simplysql_cms_devel; Type: DATABASE PROPERTIES; Schema: -; Owner: devel
+--
+
+ALTER DATABASE simplysql_cms_devel CONNECTION LIMIT = 3;
+
+
+\connect simplysql_cms_devel
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: entries; Type: TABLE; Schema: public; Owner: devel
+--
+
+CREATE TABLE public.entries (
+    id integer NOT NULL,
+    title character varying(99) NOT NULL,
+    created timestamp without time zone NOT NULL,
+    updated timestamp without time zone,
+    category character varying(37),
+    content text
+);
+
+
+ALTER TABLE public.entries OWNER TO devel;
+
+--
+-- Data for Name: entries; Type: TABLE DATA; Schema: public; Owner: devel
+--
+
+COPY public.entries (id, title, created, updated, category, content) FROM stdin;
+423	What If I Get Sick and Die?	2008-12-30 00:00:00	2009-03-11 00:00:00	angst	\N
+537	Be Nice to Everybody	2009-03-02 00:00:00	\N	advice	\N
+573	Hello Statue	2009-03-17 00:00:00	\N	humor	\N
+598	The Size of Our Galaxy	2009-04-03 00:00:00	\N	science	\N
+524	Uncle Karl and the Gasoline	2009-02-28 00:00:00	\N	humor	When I was about nine or ten, my Uncle Karl, who\nwould've been in his late teens or early twenties, once performed\nwhat to me seemed like a magic trick.\n\nUsing a rubber hose, which he snaked down into the gas tank of my \nfather's car, he siphoned some gasoline into his mouth, lit a match, \nheld it up a few inches in front of his face, and then, with explosive \nforce, sprayed the gasoline out towards the lit match.\n\nOf course, a huge fireball erupted, much to the delight of the kids\nwatching. I don't recall if he did it more than once.\n\nThe funny part of this story? We lived to tell it.\n\nKarl was like that.
+\.
+
+
+--
+-- Name: entries entries_pkey; Type: CONSTRAINT; Schema: public; Owner: devel
+--
+
+ALTER TABLE ONLY public.entries
+    ADD CONSTRAINT entries_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- src/databases-sql/dbdumps/simplysql_jointypes_devel-bkp.sql
+++ src/databases-sql/dbdumps/simplysql_jointypes_devel-bkp.sql
@@ -1,1 +1,131 @@
-pg_dump: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "root" does not exist
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.8 (Debian 14.8-1.pgdg120+1)
+-- Dumped by pg_dump version 14.8 (Debian 14.8-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: simplysql_jointypes_devel; Type: DATABASE; Schema: -; Owner: devel
+--
+
+CREATE DATABASE simplysql_jointypes_devel WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.UTF-8';
+
+
+ALTER DATABASE simplysql_jointypes_devel OWNER TO devel;
+
+\connect simplysql_jointypes_devel
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: simplysql_jointypes_devel; Type: DATABASE PROPERTIES; Schema: -; Owner: devel
+--
+
+ALTER DATABASE simplysql_jointypes_devel CONNECTION LIMIT = 3;
+
+
+\connect simplysql_jointypes_devel
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: a; Type: TABLE; Schema: public; Owner: devel
+--
+
+CREATE TABLE public.a (
+    a smallint NOT NULL
+);
+
+
+ALTER TABLE public.a OWNER TO devel;
+
+--
+-- Name: b; Type: TABLE; Schema: public; Owner: devel
+--
+
+CREATE TABLE public.b (
+    b smallint NOT NULL
+);
+
+
+ALTER TABLE public.b OWNER TO devel;
+
+--
+-- Data for Name: a; Type: TABLE DATA; Schema: public; Owner: devel
+--
+
+COPY public.a (a) FROM stdin;
+102
+104
+106
+107
+\.
+
+
+--
+-- Data for Name: b; Type: TABLE DATA; Schema: public; Owner: devel
+--
+
+COPY public.b (b) FROM stdin;
+101
+102
+104
+106
+108
+\.
+
+
+--
+-- Name: a a_pkey; Type: CONSTRAINT; Schema: public; Owner: devel
+--
+
+ALTER TABLE ONLY public.a
+    ADD CONSTRAINT a_pkey PRIMARY KEY (a);
+
+
+--
+-- Name: b b_pkey; Type: CONSTRAINT; Schema: public; Owner: devel
+--
+
+ALTER TABLE ONLY public.b
+    ADD CONSTRAINT b_pkey PRIMARY KEY (b);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
